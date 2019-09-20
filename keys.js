2dcc754a594ed6016fca8c59255a309353b8c61b
Zacharey Carey <zac4ncstate@aol.com>
2019-09-19 22:19:45 -0400

    final push

--- keys.js
+++ keys.js
@@ -1,11 +1,14 @@
 BandsInTownAPI = "https://app.swaggerhub.com/apis/Bandsintown/PublicAPI/3.0.0";
 
-console.log("this is loaded");
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
 
-// OMDbAPI = "http://www.omdbapi.com/?i=tt3896198&apikey=ec2c2c27";
-// OMBkey = ec2c2c27;
+exports.bands = {
+  app_id: process.env.BANDS
+};
+
+exports.omdbkey = {
+  key: process.env.OMDB_KEY
+};
--- liri.js
+++ liri.js
@@ -1,22 +1,29 @@
 require("dotenv").config();
 
 var axios = require("axios");
-var spotify = require("node-spotify-api");
-var spotifyKeys = require("./keys.js").spotify;
+var Spotify = require("node-spotify-api");
+var keys = require("./keys.js");
+var moment = require("moment");
+var fs = require("fs");
 
-console.log(spotifyKeys);
 var liriCommand = process.argv[2];
+// process.argv[0] = "node";
+// process.argv[1] = "liri.js";
+// var liriCommand = "concert-this";
+// var customSearch = "blackkeys";
 
 var customSearch = process.argv.slice(3).join("");
+
+var spotify = new Spotify(keys.spotify);
 
 function runLiri(liriCommand, customSearch) {
   switch (liriCommand) {
     case "concert-this":
-      getSpotify(customSearch);
+      getBandsInTown(customSearch);
       break;
 
     case "spotify-this-song":
-      getBandsInTown(customSearch);
+      getSpotify(customSearch);
       break;
 
     case "movie-this":
@@ -25,65 +32,119 @@ function runLiri(liriCommand, customSearch) {
 
     case "do-what-it-says":
       getRandom();
+      console.log("runliri");
       break;
 
     default:
-      console.log("Enter one of the commands above");
+      console.log("Enter one of the following commands");
+      console.log(
+        "concert-this, spotify-this-song, movie-this, do-what-it-says"
+      );
   }
 }
 
 function getBandsInTown(artist) {
-  var artist = customSearch;
-  var artistQueryURL =
-    "https://rest.bandsintown.com/bands/" +
-    artist +
-    "/events?app_id=f66eb880067a73f570e0d7fc347ead9b";
-
-  axios.get(artistQueryURL).then(function(response) {
-    console.log("Location: " + response.data[0].venue.city + "\r\n");
-    console.log("Date of concert");
-    console.log("Venue Name");
-  });
+  var artistQueryURL = "https://rest.bandsintown.com/artists/";
+  artistQueryURL += artist;
+  artistQueryURL += "/events?app_id=";
+  artistQueryURL += keys.bands.app_id;
+  axios
+    .get(artistQueryURL)
+    .then(function(response) {
+      for (var z = 0; z < response.data.length; z++) {
+        console.log(
+          "Date of concert: " + moment(response.data[z].datetime).format("LLLL")
+        );
+        console.log("Location: " + response.data[z].venue.city);
+        console.log("Venue Name: " + response.data[z].venue.name);
+        console.log("------------------");
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
+
+var getArtistNames = function(artist) {
+  return artist.name;
+};
 
 function getSpotify(song) {
-  var song = customSearch;
-  var songQueryURL = "" + song + "";
+  if (song === undefined) {
+    song = "back in black";
+  }
 
-  axios.get(songQueryURL).then(function(response) {
-    console.log("Song name:" + response.data);
+  spotify.search({ type: "track", query: song }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    var tracks = data.tracks.items;
+
+    for (var z = 0; z < tracks.length; z++) {
+      console.log(z);
+      console.log("artist(s): " + tracks[z].artists.map(getArtistNames));
+      console.log("song name: " + tracks[z].name);
+      console.log("preview song: " + tracks[z].preview_url);
+      console.log("album: " + tracks[z].album.name);
+      console.log("------------------");
+      var saveSongs =
+        "======Begin storing song data======" + "\nSong name:" + tracks[z].name;
+
+      fs.appendFile("./log.txt", saveSongs, err => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+      });
+    }
+
+    console.log(data);
   });
 }
 
-// spotify.search({ type: "track", query: "Back in Black" }, function(err, data) {
-//   if (err) {
-//     return console.log("Error occurred: " + err);
-//   }
+function getRandom() {
+  console.log("topofgetrandom");
+  fs.readFile("./random.txt", "utf8", function(err, data) {
+    if (err) throw err;
+    var infoarray = data.split(",");
+    console.log(infoarray);
 
-//   console.log(data);
-// });
+    if (infoarray.length === 2) {
+      runLiri(infoarray[0], infoarray[1]);
+    } else if (infoarray.length === 1) {
+      runLiri(infoarray[0]);
+    }
+  });
+  console.log("bottomofgetrandom");
+}
 
 function getOMDB(movie) {
   if (!movie) {
     movie = "Mad Max: Fury Road";
   }
-  var movieQueryUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=ec2c2c27";
-  +movie + "ec2c2c27";
+  var movieQueryUrl =
+    "http://www.omdbapi.com/?t=" + movie + "&apikey=" + keys.omdbkey.key;
 
-  axios.request(movieQueryUrl).then(function(response) {
-    console.log("Title:" + response.data.Tile + "\r\n");
-    console.log("Year Released:" + response.data.Year + "\r\n");
-    console.log("IMDB Rating:" + response.data.imbdRating + "\r\n");
-    console.log(
-      "Rotten Tomatoes Rating:" + response.data.Ratings[1].Value + "\r\n"
-    );
-    console.log("Language" + response.data.Language + "\r\n");
+  axios
+    .request(movieQueryUrl)
+    .then(function(response) {
+      console.log("Title:" + response.data.Title);
+      console.log("Year Released:" + response.data.Year);
+      console.log("IMDB Rating:" + response.data.Ratings[0].Value);
+      console.log("Rotten Tomatoes Rating:" + response.data.Ratings[1].Value);
+      console.log("Language" + response.data.Language);
 
-    var saveMovie =
-      "======Begin storing movie data======" +
-      "\nMovie title:" +
-      response.data.Title;
+      var saveMovie =
+        "======Begin storing movie data======" +
+        "\nMovie title:" +
+        response.data.Title;
 
-    fs.appendFile("random.txt", saveMovie);
-  });
+      fs.appendFile("./log.txt", saveMovie, err => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
+
+runLiri(liriCommand, customSearch);
